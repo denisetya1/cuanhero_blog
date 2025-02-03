7815e55b631b5d9a020c318bad9f70ec025b3a83
denisetya <deni.setyawan@gmail.com>
2025-02-04 00:13:03 +0700

    add favicon

--- src/app/freebook/rahasia-visa-schengen/page.tsx
+++ src/app/freebook/rahasia-visa-schengen/page.tsx
@@ -1,5 +1,11 @@
 import CollapsibleCard from '@/components/CollapsibleCard'
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Panduan Rahasia Mendapatkan Visa Schengen 99% Pasti Diterima",
+  description: "Panduan Rahasia Mendapatkan Visa Schengen 99% Pasti Diterima",
+};
 
 const Page = () => {
   return (
