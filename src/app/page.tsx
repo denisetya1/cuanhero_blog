c490cd51882088c114689c66341a3695f20b5d64
Deni <deni.setyawan@tiket.com>
2025-05-25 02:22:05 +0700

    replace redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
 
-  return redirect('/freebook/rahasia-visa-schengen');
+  return redirect('/lp/jualan-online-cuan-milyaran-tanpa-ngiklan');
 }
