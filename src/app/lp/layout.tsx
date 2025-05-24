88512e279371f5bf6a2be02113e518502d52c1a5
Deni <deni.setyawan@tiket.com>
2025-05-25 02:21:06 +0700

    add new lp

---
+++ src/app/lp/layout.tsx
@@ -0,0 +1,32 @@
+import { ThemeToggle } from '@/components/ThemeToggle'
+import React from 'react'
+import Image from 'next/image'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Panduan Rahasia Mendapatkan Visa Schengen 99% Pasti Diterima',
+  description: 'Panduan Rahasia Mendapatkan Visa Schengen 99 % Pasti Diterima',
+};
+
+const freebookLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='w-full relative pt-16'>
+      <div className='p-4 bg-black text-white w-full fixed top-0 z-50 flex justify-between items-center shadow-md'>
+        <div className='font-bold'>
+          <Image
+            color='#fff'
+            src={'/assets/img/logo.svg'}
+            width={140}
+            height={30}
+            alt='CUANHERO'
+            className='invert'
+          />
+        </div>
+        <ThemeToggle />
+      </div>
+      <div className='m-auto max-w-[760px]'>{children}</div>
+    </div>
+  )
+}
+
+export default freebookLayout
